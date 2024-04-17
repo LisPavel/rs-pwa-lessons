e8bd0bc74af2691bfc5b038f3a359b77af00f8f1
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2024-04-17 18:37:56 +0300

    feat: add initial service worker

---
+++ js/app.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) => {
+      console.log("sw registered", reg);
+    })
+    .catch((err) => {
+      console.log("sw not registered", err);
+    });
+}
