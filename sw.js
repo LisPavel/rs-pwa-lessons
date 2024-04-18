2d72c6afa765bce70c4c65d8c051746c046a9bc6
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2024-04-18 16:44:47 +0300

    feat: add activate event listener
    Refs: React/PWA/9

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
+// install sw
 self.addEventListener("install", (ev) => {
   console.log("service worker has been installed", ev);
+});
+
+// activate sw
+self.addEventListener("activate", (ev) => {
+  console.log("service worker has been activated", ev);
 });
